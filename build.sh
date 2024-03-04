63fa95a47e5122935adc73f6d4ccbe7f17512a06
Ganya-umesh <118112049+Ganya-umesh@users.noreply.github.com>
2024-03-05 00:00:42 +0530

    bash changes

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Shell script to compile a C++ file
-# Replace "YOUR_SRN" with your SRN
+# Replace "YOUR_SRN" with your actual SRN
+SRN="PES1UG21CS201"
 
 # Define the file name and output executable name
 input_file="PES1UG21CS201.cpp"
-output_executable="my_executable_201"
+output_executable="${PES1UG21CS201}-1"
 
 # Compilation command
 g++ -o $output_executable $input_file
